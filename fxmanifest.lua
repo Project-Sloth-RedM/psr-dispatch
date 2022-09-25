80cf90275def6f84f80b37f7727cb61f73a2214b
MoneSuper <62680426+MoneSuper@users.noreply.github.com>
2022-09-25 05:31:23 +0300

    psr-dispatch
    tbh idk why you need that

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+version '0.0'
+description 'https://github.com/Project-Sloth/ps-dispatch'
+
+shared_scripts {
+    'config.lua',
+    'locales/locales.lua',
+}
+
+client_scripts{
+    'client/cl_main.lua',
+    'client/cl_events.lua',
+    'client/cl_extraalerts.lua',
+    'client/cl_commands.lua',
+    'client/cl_loops.lua',
+} 
+server_script {
+    'server/sv_dispatchcodes.lua',
+    'server/sv_main.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/app.js',
+    'ui/style.css',
+}
